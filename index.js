649ffd5858c494ea197ebdde9f142ce3c446b9c0
davinci2015 <dvincija@foi.hr>
2017-02-13 08:38:23 +0100

    init

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+import React, {
+    Component,
+    View,
+    Platform
+} from 'react-native';
+
+import StatusBarSizeIOS from 'react-native-status-bar-size';
+
+class StatusBarPaddingIOS extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { height: 0 };
+    }
+
+    componentWillMount() {
+        this._renderStatusBar();
+    }
+
+    _renderStatusBar = () => {
+        if (Platform.OS == 'ios')
+            this.setState({
+                height: StatusBarSizeIOS.currentHeight
+            });
+    };
+
+    render() {
+        return (
+            <View style={{height: this.state.height}}/>
+        )
+    }
+}
+
+export default StatusBarPaddingIOS;

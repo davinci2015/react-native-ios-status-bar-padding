67baf1b0133b161a19175d2ae4f85dffe349eb63
Danijel <dvincija@foi.hr>
2017-06-24 10:25:27 +0200

    Status bar padding ios - update

--- index.js
+++ index.js
@@ -1,34 +1,5 @@
-'use strict';
-import React, {Component} from 'react';
-import {
-    View,
-    Platform
-} from 'react-native';
-
+import React from 'react';
+import {View, Platform} from 'react-native';
 import StatusBarSizeIOS from 'react-native-status-bar-size';
 
-class StatusBarPaddingIOS extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { height: 0 };
-    }
-
-    componentWillMount() {
-        this._renderStatusBar();
-    }
-
-    _renderStatusBar = () => {
-        if (Platform.OS == 'ios')
-            this.setState({
-                height: StatusBarSizeIOS.currentHeight
-            });
-    };
-
-    render() {
-        return (
-            <View style={{height: this.state.height}}/>
-        )
-    }
-}
-
-export default StatusBarPaddingIOS;
+export default () => <View style={{height: Platform.OS === 'ios' ? StatusBarSizeIOS.currentHeight : 0}}/>;
